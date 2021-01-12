42c095e53a475656f395c537cd0aee1538a09d8e
Antonio <Tenundor@gmail.com>
2021-01-12 19:20:14 +0300

    Добавил языковые поля названия покемона.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+    title_en = models.CharField(max_length=200, null=True, blank=True)
+    title_jp = models.CharField(max_length=200, null=True, blank=True)
     image = models.ImageField(
         upload_to="pokemon_images",
         null=True, blank=True
